416ba61fc42bad3d821cb50a8ca2c5a6492e3827
kjh4357 <kjh4357@gmail.com>
2022-02-16 16:50:23 +0900

    ended tailwind css layout

---
+++ pages/live/create.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from "next";
+import Layout from "../../components/layout";
+
+const Create: NextPage = () => {
+  return (
+    <Layout canGoBack title="Go Live">
+      <div className="py-10 px-4 space-y-5">
+        <div>
+          <label
+            htmlFor="name"
+            className="mb-1 block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <div className="rounded-md relative shadow-sm flex items-center">
+            <input
+              id="input"
+              type="name"
+              className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              required
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="price"
+            className="mb-1 block text-sm font-medium text-gray-700"
+          >
+            Price
+          </label>
+          <div className="rounded-md relative shadow-sm flex items-center">
+            <div className="absolute left-0 pl-3 flex items-center justify-cente pointer-events-noner">
+              <span className="text-gray-500 text-sm">$</span>
+            </div>
+            <input
+              className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              id="price"
+              type="text"
+              placeholder="0.00"
+            />
+            <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
+              <span className="text-gray-500">USD</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label className="mb-1 block text-sm font-medium text-gray-700">
+            Description
+          </label>
+          <textarea
+            rows={4}
+            className="mt-1 shadow-sm w-full focus:ring-1 focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+          />
+        </div>
+        <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+          Go live
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Create;
