808abf72c729adf5647f9a04fc7e9a2c51965253
kjh43 <kjh4357@naver.com>
2022-03-03 01:04:08 +0900

    useForm added 2

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -8,18 +8,34 @@ interface LoginForm {
 }
 
 export default function Forms() {
-	const { register, handleSubmit } = useForm<LoginForm>();
+	const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({
+		mode: "onBlur"
+	});
 	const onValid = (data: LoginForm) => {
 		console.log('valid', data)
 	}
+	console.log(errors)
 	const onInvalid = (errors: FieldErrors) => {
 		console.log('invalid', errors)
 	}
 	return (
 		<form onSubmit={handleSubmit(onValid, onInvalid)}>
 			<input {...register("username", { required: "Username is required", minLength: { message: "the username should be longer 5", value: 5 } })} type="text" placeholder="Username" />
-			<input {...register("email", { required: "email is required" })} type="email" placeholder="email" required />
+			{errors.username?.message}
+			<input
+				{...register("email", {
+					required: "email is required",
+					validate: {
+						notGmail: (value) => !value.includes("@gmail.com") ? "" : "Gmail is not allowed"
+					}
+				})}
+				type="email"
+				placeholder="email"
+				required
+			/>
+			{errors.email?.message}
 			<input {...register("password", { required: "password is required" })} type="password" placeholder="password" required />
+			{errors.password?.message}
 			<input type="submit" value="Create Account" />
 		</form>
 	);
